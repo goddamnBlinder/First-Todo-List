b0f4e3ef69f53bc85b306c57c8427037a79fc127
James Chinweike <chinweikechukwudi@gmail.com>
2022-09-25 23:07:26 +0100

    commit

--- main.js
+++ main.js
@@ -1,7 +1,58 @@
 window.addEventListener("load", () => {
 
-const form = document.querySelector("#new-task-form");
-const input = document.querySelector("#new-task-input");
-const list_element = document.querySelector("#");
+    const form = document.querySelector("#new-task-form");
+    const input = document.querySelector("#new-task-input");
+    const list_element = document.querySelector("#tasks");
+    
+    
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        
+        
+        const task = input.value;
+        
+        if (!task) {
+            alert("Is Blank 😔") 
+            return;
+        }
+        
+        form.reset();
 
+        
+    const task_element = document.createElement("div");
+    task_element.classList.add("task");
+
+    const task_content_element = document.createElement("div");
+    task_content_element.classList.add("content");
+
+    const task_input_element = document.createElement("input");
+    task_input_element.classList.add("text");
+    task_input_element.type = "text";
+    task_input_element.value = task;
+    task_input_element.setAttribute("readonly", "readonly");
+
+   //append me oh Lord
+   list_element.appendChild(task_element);
+   task_element.appendChild(task_content_element);
+   task_content_element.appendChild(task_input_element);
+
+// input.value = "";
+
+
+ const task_actions_element = document.createElement("div");
+ task_actions_element.classList("action");
+
+ const task_actions_delete = document.createElement("div");
+ task_actions_delete.classList("delete");
+
+ const task_actions_edit = document.createElement("div");
+ task_actions_edit.classList("edit");
+
+ task_actions_element.appendChild(task_actions_delete);
+ task_actions_element.appendChild()
+
+
+
+
+    });
 });
